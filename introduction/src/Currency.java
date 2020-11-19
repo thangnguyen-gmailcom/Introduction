7cbb99923dfeb0b9eb11e976da8fb406778a662b
thangnguyen001998 <thangnguyen001998@gmail.com>
2020-11-19 21:33:41 +0700

    chuyển đổi tiền tệ

--- introduction/src/Currency.java
+++ introduction/src/Currency.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 
 public class Currency {
     public static void main(String[] args) {
-        double rate = 23.000;
+        double rate = 23000;
         Scanner input = new Scanner(System.in);
-
+        System.out.println("Enter the amount to convert !");
         double USD = input.nextDouble();
-
-
+        double VND = USD * rate;
+        System.out.println("Enter in VND "+ VND);
     }
 }
